70ad0bfa8e6b39e40d6f5110de79844288d7c267
IrfanMursyid <118200463+IrfanMursyid@users.noreply.github.com>
2024-03-10 21:41:41 +0700

    Praktikum 3

---
+++ Praktikum 3/Obat.java
@@ -0,0 +1,22 @@
+package Eksepsi;
+public class Obat {
+    private String nama;
+    private boolean tersedia;
+
+    public Obat(String nama) {
+    this.nama = nama;
+    this.tersedia = true;
+ }
+    public String getNama() {
+    return nama;
+ }
+    public boolean isTersedia() {
+    return tersedia;
+ }
+    public void setTersedia(boolean tersedia) {
+    this.tersedia = tersedia;
+    }
+    // Nama : Muhammad Irfan Mursyid
+    // NIM : 24060122130046
+    // A1
+}

05500af7f7552f218c08d51990421290482aec1d
IrfanMursyid <118200463+IrfanMursyid@users.noreply.github.com>
2024-03-10 21:39:06 +0700

    Delete Praktikum 3/Tugas3 directory

--- Praktikum 3/Tugas3/Eksepsi/BatasObatPenuhException.java
+++
@@ -1,9 +0,0 @@
-package Eksepsi;
-public class BatasObatPenuhException extends Exception {
-    public BatasObatPenuhException(String message) {
-        super("Keranjang sudah penuh, tidak dapat menambahkan obat lagi"); 
-    }
-    // Nama : Muhammad Irfan Mursyid
-    // NIM : 24060122130046
-    // A1
-}
